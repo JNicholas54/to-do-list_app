e4ed03c924feeddfef22a8130903ede7f40d7e56
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-07-27 17:52:07 -0700

    improve element style, create function add items

--- scripts.js
+++ scripts.js
@@ -1,36 +1,76 @@
-function newItem () {
+let todoList = (function () {
 
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
+  let textarea = $('textarea');
+  let addButton = $('#button');
+  let map = {Enter: false, Control: false};
 
-  if (inputValue === '') {
-    alert("Please enter an item to add to the list.");
-  } else {
-    $('#list').append(li);
-  };
+  function newItem() {
 
+    let li = $('<li></li>');
+    let textareaValue = textarea.val();
+    li.append(textareaValue);
 
-function crossOut() {
-  li.toggleClass("strike");
-};
+    if (textareaValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#list').append(li);
+    }
 
-li.on("dblclick", function crossOut() {
-  li.toggleClass("strike");
-});
+    function crossOut() {
+      li.toggleClass("strike");
+    }
 
+    li.on("dblclick", crossOut);
 
-let crossOutButton = $("<crossOutButton></crossOutButton>");
-crossOutButton.append(document.createTextNode("X"));
-li.append(crossOutButton);
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append("X");
+    // quick way to check if the li background color is white or not
+    if (li.css('background')[5] === '0') {
+      crossOutButton.addClass('grey-hover');
+    }
+    li.append(crossOutButton);
 
-crossOutButton.on("click", deleteListItem);
-  function deleteListItem() {
-    li.addClass("delete");
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem() {
+      li.fadeOut(250);
+    }
+    $('#list').sortable();
   }
 
-  $('#list').sortable();
-};
+  textarea.on('keydown', e => {
+    if (e.key in map) {
+      map[e.key] = true;
+    }
+    
+    if (Object.values(map)
+    .filter(v => v === true)
+    .length === 2) {
+      newItem();
+    }
+  });
+
+  textarea.on('keyup', e => {
+    if (Object.values(map)
+    .filter(v => v === true)
+    .length === 2) {
+      textarea.val('');
+    }
+
+    if (e.key in map) {
+      map[e.key] = false;
+    }
+  });
+
+  addButton.on('click', e => {
+    textarea.val('');
+    textarea.focus();
+  })
+
+  return {
+    newItem: newItem
+  };
+
+})();
     
     
     
